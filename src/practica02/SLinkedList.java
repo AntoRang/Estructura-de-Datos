67645f9b2f062d3580f227108b2f1ca49549d376
Antonio Rangel <AntonioRang@MacBook-Pro-de-Antonio.local>
2017-08-26 12:29:05 -0500

    Practica02 faltan los métodos set y toArray

--- src/practica02/SLinkedList.java
+++ src/practica02/SLinkedList.java
@@ -5,29 +5,29 @@ import java.util.NoSuchElementException;
 /**
  * Created by AntonioRang on 8/24/17.
  */
-public class SLinkedList<E> {
 
-    private SNode<E> top;
-    private int size;
+public class SLinkedList<E> implements List<E> {
+	private SNode<E> top;
+	private int size;
+	
+	public SLinkedList() {
+		top = new SNode<E>();
+		size = 0;
+	}
 
-    public SLinkedList() {
-        top = new SNode<E>();
-        size = 0;
-    }
 
-
-    public void addFirst(E e) {
-
+	@Override
+	public void addFirst(E e) {
+		// TODO Auto-generated method stub
         SNode<E> newNode = new SNode<E>(e);
         newNode.next = top.next;
         top.next = newNode;
         size++;
+	}
 
-    }
-
-
-    public void addLast(E e) {
-
+	@Override
+	public void addLast(E e) {
+		// TODO Auto-generated method stub
         SNode<E> newNode= new SNode<E>(e);
 
         SNode<E> current = top;
@@ -37,30 +37,39 @@ public class SLinkedList<E> {
 
         current.next = newNode;
         size++;
+	}
 
-    }
-
-
-    public void add(int index, E element) {
-        // TODO Auto-generated method stub
-
-        if(index < 0 || index > size()){
+	@Override
+	public void add(int index, E element) {
+		// TODO Auto-generated method stub
+        if(index < 0 || index > size()) {
             throw new IndexOutOfBoundsException();
         }
 
-
+        SNode<E> current = top;
         SNode<E> newNode = new SNode<E>(element);
-        newNode.next = top.next;
 
+        if(top.next != null && index ==0){
+            newNode.next = top.next;
+            top.next = newNode;
+            size++;
+        }else{
+            for(int i=0; i<index; i++){
+                current = current.next;
 
-    }
-
-
-    public E removeFirst() {
-
-        if(top.next == null){
-            throw new NoSuchElementException();
+            }
+            SNode<E> x = current.next;
+            current.next = newNode;
+            newNode.next = x;
+            size++;
         }
+	}
+
+	@Override
+	public E removeFirst() {
+		// TODO Auto-generated method stub
+        if(top.next==null)
+            throw new NoSuchElementException();
 
         SNode<E> nodeToRemove = top.next;
         top.next = nodeToRemove.next;
@@ -69,18 +78,18 @@ public class SLinkedList<E> {
 
         return nodeToRemove.value;
 
-    }
+	}
 
+	@Override
+	public E removeLast() {
+		// TODO Auto-generated method stub
 
-    public E removeLast() {
-
-        if(top.next == null){
+        if(top.next==null)
             throw new NoSuchElementException();
-        }
 
         SNode<E> current = top;
 
-        while(current.next.next!= null){
+        while(current.next.next != null){
             current = current.next;
         }
 
@@ -88,33 +97,82 @@ public class SLinkedList<E> {
         current.next = null;
         size--;
         return lastElement.value;
-    }
+	}
+
+	@Override
+	public E remove(int index) {
+		// TODO Auto-generated method stub
+        if(index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        SNode<E> current = top;
 
 
-    public E remove(int index) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+        if(current.next != null && index ==0){
+            SNode<E> x = current.next;
+            top.next =null;
+            size--;
+            return x.value;
+        }else{
+            for(int i=0; i<index; i++){
+                current = current.next;
 
+            }
 
-    public boolean remove(Object o) {
-        // TODO Auto-generated method stub
+            SNode<E> x = current;
+            SNode<E> a = x.next.next;
+            current.next = a;
+
+            size--;
+            return x.value;
+        }
+
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+        int index=0;
+        if (o == null) {
+            for (SNode<E> x = top.next; x != null; x = x.next) {
+                if (x.value == null) {
+                    remove(index);
+                    size--;
+                    return true;
+                }
+
+                index++;
+            }
+        }else{
+
+            for(SNode<E> x = top.next; x != null; x= x.next){
+                if(o.equals(x.value)) {
+                    remove(index);
+                    size--;
+                    return true;
+                }
+                index++;
+            }
+        }
+
         return false;
-    }
 
+	}
 
-    public E getFirst() {
-
+	@Override
+	public E getFirst() {
+		// TODO Auto-generated method stub
         if(top.next == null){
             throw new NoSuchElementException();
         }
 
         return top.next.value;
-    }
+	}
 
-
-    public E getLast() {
-
+	@Override
+	public E getLast() {
+		// TODO Auto-generated method stub
         if(top.next == null){
             throw new NoSuchElementException();
         }
@@ -125,78 +183,102 @@ public class SLinkedList<E> {
         }
 
         return current.value;
-    }
+	}
+
+	@Override
+	public E get(int index) {
+		// TODO Auto-generated method stub
+
+        if(index < 0 || index > size()) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        SNode<E> current = top;
+
+        for (int i = 0; i <= index ; i++) {
+            current = current.next;
+
+        }
+
+        return current.value;
+	}
 
 
-    public E get(int index) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public E set(int index, E element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	public boolean contains(E e) {
+		// TODO Auto-generated method stub
+        SNode<E> current = top;
 
-    public E set(int index, E element) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+        for (int i=0; i <= size();i++){
+            if(indexOf(e) == i){
+                return true;
+            }
+            current = current.next;
+        }
 
+		return false;
+	}
 
-    public boolean contains(E e) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-
-    public int indexOf(Object o) {
-
-        int index =0;
-        if (o == null){
-            for(SNode<E> x =top.next; x != null; x = x.next){
-                if(x.value == null) return index;
+	@Override
+	public int indexOf(Object o) {
+		// TODO Auto-generated method stub
+		int index=0;
+        if (o == null) {
+            for (SNode<E> x = top.next; x != null; x = x.next) {
+                if (x.value == null) {
+                    return index;
+                }
 
                 index++;
             }
         }else{
 
-            for(SNode<E> x = top.next; x!= null; x=x.next){
-                if(o.equals(x.value)){
+            for(SNode<E> x = top.next; x != null; x= x.next){
+                if(o.equals(x.value)) {
                     return index;
                 }
                 index++;
+
             }
         }
 
         return -1;
-    }
+	}
 
-
-    public void clear() {
-        // TODO Auto-generated method stub
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
         top.next = null;
         size=0;
+	}
 
-    }
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return size;
+	}
 
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return size()==0;
+	}
 
-    public int size() {
-        // TODO Auto-generated method stub
-        return size;
-    }
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 
-
-    public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        return size() ==0;
-    }
-
-
-    public Object[] toArray() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    public String toString() {
-
+	public String toString() {
+		// TODO Auto-generated method stub
         if(top.next == null) return "[]";
 
         SNode<E> current = top.next;
@@ -209,5 +291,6 @@ public class SLinkedList<E> {
 
         returnValue += "]";
         return returnValue;
-    }
+	}
 }
+
